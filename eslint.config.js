b1c290aebe2431de09e975f4bdaecc8a25c36a75
Odin@uepg24 <odinuepg2024@gmail.com>
2024-08-17 23:24:26 -0300

    chore: change eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import tseslint from 'typescript-eslint';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import reactPlugin from 'eslint-plugin-react';
 import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 
 const config = [
@@ -23,16 +24,20 @@ const config = [
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+  },
+  { ...eslintPluginPrettier },
+  { ...reactPlugin.configs.flat.recommended },
+  {
     rules: {
       ...reactHooks.configs.recommended.rules,
+      'no-unused-vars': ['off'],
+      'react-hooks/exhaustive-deps': ['off'],
+      'react/react-in-jsx-scope': ['off'],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
     },
-  },
-  {
-    ...eslintPluginPrettier,
   },
 ];
 
