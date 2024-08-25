e49d867189523f7161d224e1bed8ece2de7e1242
Odin@uepg24 <odinuepg2024@gmail.com>
2024-08-25 20:12:24 -0300

    refactor: change router

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './routes/router.tsx';
+import { router } from './router/root';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
---
+++ src/router/root.tsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from 'react-router-dom';
+import App from '../App';
+import { Login } from '../pages/sign-in/login';
+import { ScheduleForm } from '../pages/schedule/schedule-form';
+import { PageNotFound } from '../pages/error/page-not-found';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <PageNotFound />,
+    children: [
+      {
+        path: '/',
+        element: <Login />,
+      },
+      {
+        path: '/new-schedule',
+        element: <ScheduleForm />,
+      },
+      {
+        path: '/update-schedule',
+        element: <ScheduleForm />,
+      },
+    ],
+  },
+]);
