243c3190bd3f39cba01a34bc448a08c6659cb4c7
Odin-Projeto <odinuepg2024@gmail.com>
2024-08-27 17:51:25 -0300

    Update root.tsx

--- src/router/root.tsx
+++ src/router/root.tsx
@@ -10,7 +10,7 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <PageNotFound />,
+    //errorElement: <PageNotFound />,
     children: [
       {
         path: '/',
